1f8975616ea52d28f039e332d89e35d2f4733271
Brandon McDonald <bmcdonald@tmlirp.org>
2018-05-14 16:21:57 -0500

    Added instance and methods practice as well as beginning of Constructors practice

--- JavaBasics/src/basics/methodsAndInstances.java
+++ JavaBasics/src/basics/methodsAndInstances.java
@@ -1,10 +1,19 @@
 package basics;
-
+/* In this class we are creating simple methods that we 
+ * will be referencing in another class (name.java). Please take
+ * note of the getter and setter method we have in this class for 
+ * future reference.
+ */
 public class methodsAndInstances {
-	
-	private String girlName;
 
-	public static void main(String[] args) {
-		
+	private String girlName;
+	public void setName(String name) {
+		girlName=name;
+	}
+	public String getName() {
+		return girlName;
+	}
+	public void saying() {
+		System.out.printf("Your first gf was %s", getName());
 	}
 }
--- JavaBasics/src/basics/name.java
+++ JavaBasics/src/basics/name.java
@@ -1,5 +1,17 @@
 package basics;
 
+import java.util.Scanner;
+/* In this class we are referencing 
+ * our methods that we created in the 'methodsAndInstances.java' class
+ */
 public class name {
 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		methodsAndInstances object = new methodsAndInstances();
+		System.out.println("enter name of first gf here: ");
+		String temp = input.nextLine();
+		object.setName(temp);
+		object.saying();
+	}
 }
