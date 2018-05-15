1ccca591388d2348afc2b9461190ea9e15fb3fec
Brandon McDonald <bmcdonald@tmlirp.org>
2018-05-15 11:13:45 -0500

    while loops updated comment

--- JavaBasics/src/basics/whileLoops.java
+++ JavaBasics/src/basics/whileLoops.java
@@ -2,7 +2,8 @@ package basics;
 
 public class whileLoops {
 /*
- * Here is an example of a while loop. This prints
+ * Here is an example of a while loop. This prints out numbers 1 - 20.
+ * Notice that the counter variable is inside the while loop block.
  */
 	public static void main(String[] args) {
 		int counter = 0;
