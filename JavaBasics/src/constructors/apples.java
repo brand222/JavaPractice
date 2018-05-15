cd6899edc7644a9e1ad298afabfad72fd60ed73e
Brandon McDonald <bmcdonald@tmlirp.org>
2018-05-15 11:03:14 -0500

    Add stuff

--- JavaBasics/src/constructors/apples.java
+++ JavaBasics/src/constructors/apples.java
@@ -3,16 +3,11 @@ package constructors;
 public class apples {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		tuna tunaObject = new tuna();
-		tuna tunaObject2 = new tuna();
+		tuna tunaObject = new tuna("Cindy");
+		tuna tunaObject2 = new tuna("Samantha");
 		tunaObject.tuna("Cindy");
 		tunaObject.saying();
-		tunaObject2.tuna("Samantha");
-=======
-		tuna tunaObject = new tuna("Kelsey)");
->>>>>>> 97c3cdad68811dec95ce8a5c38e0c036404ae5a4
-		tunaObject.saying();
+		tunaObject2.saying();
 	}
 
 }
