3fe193acee979ea7a55df58506e59af4343cede5
brand222 <Brandon_mcDonald22@yahoo.com>
2018-05-22 23:05:19 -0500

    added GUI stuff and polymorphism part 2 stuff

--- JavaBasics/src/classToHoldObjects/apples.java
+++ JavaBasics/src/classToHoldObjects/apples.java
@@ -2,4 +2,14 @@ package classToHoldObjects;
 
 public class apples {
 
+	public static void main(String[] args) {
+		//here we create out objects
+		animalList ALO = new animalList();
+		dog d = new dog();
+		fish f = new fish();
+		ALO.add(d);
+		ALO.add(f);
+		
+		
+	}
 }
