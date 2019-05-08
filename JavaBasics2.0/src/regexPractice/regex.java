e03d202c49ab4841ae1f1aa0f1b3fa587270633b
brand222 <Brandon_mcDonald22@yahoo.com>
2019-05-08 18:55:36 -0500

    added some regex stuff.

--- JavaBasics2.0/src/regexPractice/regex.java
+++ JavaBasics2.0/src/regexPractice/regex.java
@@ -6,61 +6,54 @@ public class regex {
 
 	public static void main(String[] args) {
 		
-		String longString = "Brandon McDonald TX  212-293-9495 Feathergrass Ct Austin brandonmcdonald FE BR 713 829 3638 (713-493-2348";
+		String s = "City: [name:NYK][distance:1100] [name:CLT][distance:2300] [name:KTY][distance:3540] Price:";
+		String namesString = "brandon mcdonald, jennifer collete, jen bateman, jenny collete";
+
+		String longString = "Brandon McDonald TX  212-293-9495 33220 Feathergrass 123 Main St. Ct Austin brandonmcdonald FE BR 713 829 3638 (713-493-2348";
 		String strangeString = "1Z aaa **** ** {{{ {{ { ";
-		
-		
-		
-		//[A-Z]
-		//[0-9]
-		//[^A-G]
-		//\\s - whitespace
-		//{n,m}
-		
-		//looking for a word that is 2 - 20 characters in length 
-		//[A-Za-z]{2,20} 	\\w{2,20
-		
-		//regexChecker("\\s[A-Za-z]{2,20}\\s", longString);
-		
-		//regexChecker("\\s\\d{5}\\s", longString);
-		
-		//2 characters that start with a F or A (with the next letter being: E or R)
-		//F[E]|B[R]
-		
-		//regexChecker("F[E]|B[R]", longString);
-		
-		//get the minimum and maximum
-		
-		//{5,}
-		// +
+
+		// [A-Z]
+		// [0-9]
+		// [^A-G]
+		// \\s - whitespace
+		// {n,m}
+		/*
+		 * \d = any number \D = anything but a number \s = any space \S =
+		 * anything but a space \w = any character \W = anything but a character
+		 * . = any character except for a line break \b = a space that preceeds
+		 * or follows a whole word ? = 0 or 1 repetitions * = 0 or more
+		 * repetitions {n} = expect {n} number of times
+		 * 
+		 * Escaping: ex. searching for a dollar amount \$\d*\.\d{2} = search for
+		 * a dollar sign followed by 0 or more numbers and then a period and a
+		 * digit with a 2 length
+		 */
 		// . ^ * + ?{} [] \ | () - these always need to be backslashed
 		
-		//regexChecker("(\\{{1,})", strangeString);
+		regexChecker("\\[name:([A-Z]+)\\]\\[distance:(\\d+)\\]", s);
 		
-		//regexChecker("\\sjohnsmith@hotmail.com", longString);
-		
-		//extracts all the phone numbers (regardless spaces and dashes)
-		regexChecker("(\\d+-\\d+-\\d+|\\d+\\s+\\d+-\\d+|\\d+\\s+\\d+\\s+\\d+)", longString);
-		//extracts states
-		//regexChecker("\\d.+\\s", strangeString);
+		//searches for all the jen-based names (full names)
+		//regexChecker("\\s+je(n|nnifer|nny)\\s\\w+\\s*", namesString);
+
+		// extracts all the phone numbers (regardless spaces and dashes)
+		//regexChecker("(\\d+-\\d+-\\d+|\\d+\\s+\\d+-\\d+|\\d+\\s+\\d+\\s+\\d+)", longString);
 	}
-	
+
 	public static void regexChecker(String theRegex, String str2Check) {
-		//here we are creating a pattern where we pass in the string parameter
+		// here we are creating a pattern where we pass in the string parameter
 		Pattern checkRegex = Pattern.compile(theRegex);
-		//here we create a matcher variable
+		// here we create a matcher variable
 		Matcher regexmatcher = checkRegex.matcher(str2Check);
-		//while 
-		while(regexmatcher.find()){
-			//if it has a value and a length
+		// while
+		while (regexmatcher.find()) {
+			// if it has a value and a length
 			if (regexmatcher.group().length() != 0) {
-				//we will get rid of white space
+				// we will get rid of white space
 				System.out.println(regexmatcher.group().trim());
 			}
-			//print out the starting and ending index
-			System.out.println("Start index: " + regexmatcher.start());
-			System.out.println("End index:" + regexmatcher.end());
+			// print out the starting and ending index
+			//System.out.println("Start index: " + regexmatcher.start());
+			//System.out.println("End index:" + regexmatcher.end());
 		}
 	}
 }
-
